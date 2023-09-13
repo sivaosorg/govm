710a190df110f1f5d760bdc89ad0fedda82695c9
pnguyen215 <nguyenhuuphuocit97@gmail.com>
2023-09-13 17:16:47 +0700

    :chart_with_upwards_trend: perf: improve performance #16

--- configx/configx.go
+++ configx/configx.go
@@ -1,13 +1,35 @@
 package configx
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
 
+	"github.com/sivaosorg/govm/utils"
 	"gopkg.in/yaml.v2"
 )
 
-func NewConfig[T any](path string) (*T, error) {
+func NewCommentedConfig() *CommentedConfig {
+	c := &CommentedConfig{}
+	return c
+}
+
+func (c *CommentedConfig) SetData(value interface{}) *CommentedConfig {
+	c.Data = value
+	return c
+}
+
+func (c *CommentedConfig) SetComment(value FieldCommentConfig) *CommentedConfig {
+	c.Comments = value
+	return c
+}
+
+func (c *CommentedConfig) Json() string {
+	return utils.ToJson(c)
+}
+
+func ReadConfig[T any](path string) (*T, error) {
 	file, err := os.Open(path)
 	if err != nil {
 		return nil, err
@@ -23,4 +45,59 @@ func NewConfig[T any](path string) (*T, error) {
 		return nil, err
 	}
 	return &cfg, nil
+}
+
+func CreateConfig[T any](path string, data *T) error {
+	config, err := yaml.Marshal(data)
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(path, config, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func CreateConfigWithComments[T any](path string, data CommentedConfig) error {
+	bytes, err := _marshal(data.Data, data.Comments)
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(path, bytes, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func _marshal(data interface{}, comments FieldCommentConfig) ([]byte, error) {
+	bytes, err := yaml.Marshal(data)
+	if err != nil {
+		return nil, err
+	}
+	lines := strings.Split(string(bytes), "\n")
+	for field, comment := range comments {
+		for i, line := range lines {
+			if strings.Contains(line, field+":") {
+				if strings.Contains(comment, "\n") {
+					commentLines := strings.Split(comment, "\n")
+					for j := len(commentLines) - 1; j >= 0; j-- {
+						commentLine := fmt.Sprintf("# %s", commentLines[j])
+						lines = insertStringAt(lines, i, commentLine)
+					}
+				} else {
+					commentLine := fmt.Sprintf("# %s", comment)
+					lines = insertStringAt(lines, i, commentLine)
+				}
+				break
+			}
+		}
+	}
+	c := strings.Join(lines, "\n")
+	return []byte(c), nil
+}
+
+func insertStringAt(slice []string, index int, value string) []string {
+	return append(slice[:index], append([]string{value}, slice[index:]...)...)
 }
--- configx/configx_model.go
+++ configx/configx_model.go
@@ -1,1 +1,8 @@
 package configx
+
+type FieldCommentConfig map[string]string
+
+type CommentedConfig struct {
+	Data     interface{}        `json:"data" binding:"required" yaml:"-"`
+	Comments FieldCommentConfig `json:"comments" yaml:"-"`
+}
